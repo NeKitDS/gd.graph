e7e2a4e7d2c0631e296975d288fa1cbb966e092b
Nikita Tikhonov <nekitdevofficial@gmail.com>
2021-02-07 20:48:09 +0300

    [0.1.1] Small copyright updates.

--- gd/graph/__init__.py
+++ gd/graph/__init__.py
@@ -2,6 +2,6 @@
 
 __title__ = "graph"
 __author__ = "nekitdev"
-__copyright__ = "Copyright 2020 nekitdev"
+__copyright__ = "Copyright 2020-2021 nekitdev"
 __license__ = "MIT"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
